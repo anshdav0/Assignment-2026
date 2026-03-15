dfff19198618991756c3a8475e912a395a2284dd
ansh <anshdav0@gmail.com>
2026-03-15 14:28:54 +0000

    Changed Monkeytype application, verified GPG Key and changed commit names.
    GPG Key was not added to Github previously, now it is.
    Monkeytype now compare word by word so that the Accuracy remains correct. Also wrong input while typing are now shown red while correct one red. Added difficulty levels.
    Added more database files for difficulty maintainence.

--- Monkeytype/monkey.sh
+++ Monkeytype/monkey.sh
@@ -1,72 +1,176 @@
 #!/bin/bash
-RANDOM=$$
+
+compare_arrays() {
+	local -n inp=$1
+	local -n opt=$2
+	total_points=0
+	len_inp=0
+	len_opt=0
+	local i=0
+
+	while [ $i -lt ${#inp[@]} ]; do
+		w1="${inp[$i]}"
+		w2="${opt[$i]}"
+		local l1=${#w1}
+		local l2=${#w2}
+		((len_inp += l1))
+		((len_opt += l2))
+
+		if [ $l1 -le $l2 ]; then
+			local l=$l1
+		else
+			local l=$l2
+		fi
+
+		local j=0
+		while [ $j -lt $l ]; do
+			c1="${w1:$j:1}"
+			c2="${w2:$j:1}"
+
+			if [ "$c1" = "$c2" ]; then
+				((total_points++))
+			fi
+			((j++))
+		done
+		((i++))
+	done
+
+}
 
 game() {
 	echo "Enter words which are displayed on the screen and in the end press enter"
 
-	dis=""
-	inpt=""
-	DFM="database_for_monkey.txt"
+	while true; do
+		read -p "Choose gamemode (1 min Timed(t)/Word(w)): " mode
+		if [ "$mode" = 't' ]; then
+			time=60
+			break
+		elif [ "$mode" = 'w' ]; then
+			time=600
+			break
+		fi
+	done
+
+	dis=()
+	dis_str=""
+	inpt=()
+	inpt_str=""
+	while true; do
+		read -p "Difficulty (Easy(e)/Medium(m)/Hard(h)): " diff
+		if [ "$diff" = 'e' ]; then
+			DFM="database_for_monkey.txt"
+			break
+		elif [ "$diff" = 'm' ]; then
+			DFM="database_for_monkey_med.txt"
+			break
+		elif [ "$diff" = 'h' ]; then
+			DFM="database_for_monkey_hard.txt"
+			break
+		fi
+	done
+
 	nowd=13
 	nowid=100
 
 	i=1
 	while [ $i -le $nowd ]; do
-	    n=$((RANDOM % $nowid))
-	    if [ -z "$dis" ]; then
-		dis="$(sed -n "${n}p" "$DFM")"
-	    else
-    		dis="$dis $(sed -n "${n}p" "$DFM")"
-	    fi
+
+	    n=$((RANDOM % $nowid + 1))
+	    word="$(sed -n "${n}p" "$DFM")"
+	    dis+=("$word")
 
 	    ((i++))
 	done
+	dis_str="${dis[*]}"
 
 	echo ""
 	echo "Type below sentence:"
-	echo "${dis}"
+	echo "${dis[@]}"
 
+
+	sentence=""
+	pos=0
+	i=0
+	j=0
 	SECONDS=0
-	read -p "Enter a sentence: " sentence
-	time_type=$SECONDS
 
-	inpt=$sentence
-
-	total=${#dis}
-	inpt_len=${#inpt}
-
-	if [ $total -le $inpt_len ]; then
-		len=$total
-	else
-		len=$inpt_len
-	fi
-	k=0
-	points=0
-
-	while [ $k -lt $len ]; do
-		char1="${dis:$k:1}"
-		char2="${inpt:$k:1}"
-
-		if [ "$char1" = "$char2" ]; then
-			((points++))
+	while IFS= read -r -s -n1 char; do
+		if [ $mode == 't' ]; then
+			if [ $SECONDS -ge 60 ]; then
+				time_type=$SECONDS
+				echo ""
+				break
+			fi
 		fi
 
-		((k++))
+		if [[ -z "$char" ]]; then
+			time_type=$SECONDS
+			echo ""
+			break
+		fi
+
+		if [[ "$char" == $'\x7f' ]]; then
+			if [ ${#sentence} -gt 0 ]; then
+				sentence="${sentence%?}"
+				if [ $i -eq 0 ]; then
+					((j--))
+					i=$((lastno + 1))
+				fi
+				((i--))
+				echo -ne "\b \b"
+			fi
+
+		elif [[ "$char" == " " ]]; then
+			sentence+=" "
+			echo -n " "
+			((j++))
+			lastno=$i
+			i=0
+			if [ $mode == 'w' ]; then
+				if [ $j -ge $nowd ]; then
+					time_type=$SECONDS
+					break
+				fi
+			fi
+
+		else
+			sentence+="$char"
+			exp="${dis[$j]:$i:1}"
+			if [ "$char" = "$exp" ]; then
+				tput setaf 2
+			else
+				tput setaf 1
+			fi
+			echo -n "$char"
+			tput sgr0
+			((i++))
+		fi
 	done
+	inpt=($sentence)
+	compare_arrays inpt dis
+	if [ $len_inp -eq 0 ]; then
+		echo "Entered nothing"
+	else
+		accuracy=$(echo "scale=2; ($total_points*100)/$len_inp" | bc)
+		echo "Accuracy: $accuracy%"
 
-	accuracy=$(echo "scale=2; ($points*100)/$inpt_len" | bc)
-	echo "Accuracy: $accuracy%"
-
-	wpm=$(echo "scale=2; ($inpt_len*60)/$total" | bc)
-	echo "WPM: $wpm"
+		wpm=$(echo "scale=2; ($len_inp*12)/$time_type" | bc)
+		echo "WPM: $wpm"
+	fi
 	}
 
 	while true; do
 		game
-		echo "Continue the test!! (YES(Y)/NO(N)): "
-		read game_state
-		if [ $game_state == 'N' ]
-		then
-		    break
+		while true; do
+			read -p "Continue the test!! (YES(Y)/NO(N)): " game_state
+
+			if [ "$game_state" = 'Y' ]; then
+		    		break
+			elif [ "$game_state" = "N" ]; then
+				break
+			fi
+		done
+		if [ "$game_state" = "N" ]; then
+			break
 		fi
 	done
